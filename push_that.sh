8c5a049203c7c2329c80956bdf980e784fa252f7
Noure Chatti <noure@MacBook-Air-de-Noure.local>
2025-10-10 15:54:03 +0200

    message par défaut

--- push_that.sh
+++ push_that.sh
@@ -3,8 +3,8 @@
 git add $(find . -maxdepth 1 -type f)
 
 if [ -z "$1" ]
- then git commit "message par défaut"
- else git commit "$1"
+ then git commit -m "message par défaut"
+ else git commit -m "$1"
 fi 
 
 git push 
